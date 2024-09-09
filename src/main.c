0a77c73f7196e7a7ce656f535ed60f19e719c958
Ian Romanick <idr@paranormal-entertainment.com>
2024-09-09 12:02:33 -0700

    Fix fractional second time logging
    Previously a time of 14.002 would have been incorrectly printed as
    14.2. This commit also moves logging the expected time to before the
    song starts playing. This gives the user some idea how long it will
    play.

--- src/main.c
+++ src/main.c
@@ -815,17 +815,17 @@ main(int argc, char **argv)
 
     calibrate_delay();
 
+    uint32_t expected_ms = (10 * header.total_samples) / 441;
+    printf("Expected play time = %lu.%03lus (%lu samples @ 44100Hz)\n",
+           expected_ms / 1000, expected_ms % 1000,
+           header.total_samples);
+
     uint32_t before = get_tick();
     play_Tandy_sound(&v, &header);
     uint32_t after = get_tick();
 
-    uint32_t expected_ms = (10 * header.total_samples) / 441;
-    printf("Expected play time = %lu.%lus (%lu samples @ 44100Hz)\n",
-           expected_ms / 1000, expected_ms % 1000,
-           header.total_samples);
-
     uint32_t elapsed_ms = 55ul * (after - before);
-    printf("Elapsed play time = %lu.%lus (%lu ticks)\n",
+    printf("Elapsed play time = %lu.%03lus (%lu ticks)\n",
            elapsed_ms / 1000, elapsed_ms % 1000,
            after - before);
 
